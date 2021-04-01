4a33023c85e6a73c91a875b4e3e1e0defa4bd646
psw221 <tpdnddlek@gmail.com>
2021-04-01 22:28:25 +0900

    add shop-page and collection components

--- src/App.js
+++ src/App.js
@@ -3,19 +3,14 @@ import { Route, Switch } from 'react-router';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
-
-const HatsPage = () => {
-  return (
-    <div>HATS PAGE</div>
-  )
-};
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route path='/shop/hats' component={HatsPage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
