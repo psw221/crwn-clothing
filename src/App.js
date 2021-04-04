5be759d6949b9ab4a39f91ec64369fe53252aeb3
psw221 <tpdnddlek@gmail.com>
2021-04-04 23:08:13 +0900

    add and set eslint prettier package

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import { Route, Switch } from 'react-router';
-import './App.css';
+import React from "react";
+import { Route, Switch } from "react-router";
+import "./App.css";
 
-import HomePage from './pages/homepage/homepage.component';
-import ShopPage from './pages/shop/shop.component';
+import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
 
 function App() {
   return (
     <div>
       <Switch>
-        <Route exact path='/' component={HomePage} />
-        <Route path='/shop' component={ShopPage} />
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
