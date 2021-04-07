4fcd71917d7c8e3fe46eadc7e6e21527b679fba4
psw221 <tpdnddlek@gmail.com>
2021-04-07 22:29:47 +0900

    add form input component

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,48 +1,62 @@
-import React from 'react';
+import React from "react";
 
-import './sign-in.styles.scss';
+import FormInput from "../form-input/form-input.component";
+
+import "./sign-in.styles.scss";
 
 class SignIn extends React.Component {
-    constructor() {
-        super();
+  constructor() {
+    super();
 
-        this.state= {
-            email: '',
-            password: ''
-        };
-    }
+    this.state = {
+      email: "",
+      password: "",
+    };
+  }
 
-    handleSubmit = event => {
-        event.preventDefault();
+  handleSubmit = (event) => {
+    event.preventDefault();
 
-        this.setState({
-            email: '', password:''
-        });
-    }
+    this.setState({
+      email: "",
+      password: "",
+    });
+  };
 
-    handleChange = event => {
-        const { value, name } = event.target;
+  handleChange = (event) => {
+    const { value, name } = event.target;
 
-        this.setState({ [name]: value })
-    }
+    this.setState({ [name]: value });
+  };
 
-    render() {
-        return(
-            <div className='sign-in'>
-                <h2>I already have an account</h2>
-                <span>Sign in with your email and password</span>
+  render() {
+    return (
+      <div className="sign-in">
+        <h2>I already have an account</h2>
+        <span>Sign in with your email and password</span>
 
-                <form onSubmit={this.handleSubmit}>
-                    <input name="email" type='email' value={this.state.email} onChange={this.handleChange} required />
-                    <label>Email</label>
-                    <input name="password" type='password' value={this.state.password} onChange={this.handleChange} required />
-                    <label>Password</label>
-
-                    <input type="submit" value="Submit Form" />
-                </form>
-            </div>
-        )
-    }
-};
+        <form onSubmit={this.handleSubmit}>
+          <FormInput
+            name="email"
+            type="email"
+            value={this.state.email}
+            handleChange={this.handleChange}
+            label="email"
+            required
+          />
+          <FormInput
+            name="password"
+            type="password"
+            value={this.state.password}
+            label="password"
+            handleChange={this.handleChange}
+            required
+          />
+          <input type="submit" value="Submit Form" />
+        </form>
+      </div>
+    );
+  }
+}
 
 export default SignIn;
--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -1,9 +1,11 @@
-import React from 'react';
-import SignIn from '../../components/sign-in/sign-in.component';
-import './sign-in-and-sign-up.styles.scss';
+import React from "react";
+import SignIn from "../../components/sign-in/sign-in.component";
+import "./sign-in-and-sign-up.styles.scss";
 
-const SignInAndSignUpPage= () => (
-    <SignIn/>
+const SignInAndSignUpPage = () => (
+  <div className="sign-in-and-sign-up">
+    <SignIn />
+  </div>
 );
 
 export default SignInAndSignUpPage;
