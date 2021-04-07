6d035f8419f0c89ac2fc470e84b4de81dd201874
psw221 <tpdnddlek@gmail.com>
2021-04-07 21:47:29 +0900

    add sign in and sign up page and sign in component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
-import React from "react";
-import { Route, Switch } from "react-router";
-import "./App.css";
+import React from 'react';
+import {
+  Route,
+  Switch,
+} from 'react-router';
+import './App.css';
 
-import HomePage from "./pages/homepage/homepage.component";
-import ShopPage from "./pages/shop/shop.component";
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 function App() {
   return (
     <div>
       <Header />
       <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/shop" component={ShopPage} />
+        <Route
+          exact
+          path="/"
+          component={HomePage}
+        />
+        <Route
+          path="/shop"
+          component={ShopPage}
+        />
+        <Route
+          path="/signin"
+          component={
+            SignInAndSignUpPage
+          }
+        />
       </Switch>
     </div>
   );
---
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import SignIn from '../../components/sign-in/sign-in.component';
+import './sign-in-and-sign-up.styles.scss';
+
+const SignInAndSignUpPage= () => (
+    <SignIn/>
+);
+
+export default SignInAndSignUpPage;
